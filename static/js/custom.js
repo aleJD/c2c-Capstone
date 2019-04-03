6406f182e794e46d0cc9e5640202b55aa9f6ead2
aleJD <alejandrodoberenz.school@gmail.com>
2019-04-03 22:30:29 +0000

    Javascript stuff

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,23 @@
-var clicks = 0;
+var textFields = document.getElementsByClassName("classTextField");
+var parsedValues = [textFields.length];
 
 function submit() {
-    clicks++;
-    document.getElementById("nein").innerHTML = "lmao" + clicks;
-    alert("i changed this: " + clicks);
+    for(int i = 0; i < textFields.length; i++) {
+        var parVal = textFields[i].value;
+        parVal = parseInt(parVal);
+        if(parVal.isNaN) {
+            return;
+        } else {
+            parsedValues[i] = parsedValues;
+        }
+    }
+    var sum = 0;
+    for(i = 0; i < textFields.length; i++) {
+        sum += textFields[i].value;
+    }
+    alert("complete, sum: " + sum);
+}
+
+function invalid() {
+
 }
